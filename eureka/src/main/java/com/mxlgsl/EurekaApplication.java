1ad47c32c3328ac876b4c5209c7f95f09765aa04
程达 <mxlgslcd@163.com>
2019-12-31 14:58:37 +0800

    修改

--- eureka/src/main/java/com/mxlgsl/EurekaApplication.java
+++ eureka/src/main/java/com/mxlgsl/EurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //声明自己是一个Eurekaserver
 public class EurekaApplication {
 
     public static void main(String[] args) {
